49b2d90de9047ad2a9e049521bb404003a17410c
arsenalmorimori <arsenalbautista1@gmail.com>
2025-12-03 09:56:42 +0800

    firsttt push

--- Test/Program.cs
+++ Test/Program.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Test {
+    internal class Program {
+        static void Main(string[] args) {
             //something
+            for (int i = 0; i < 100; i++) {
+                Console.WriteLine("HELOOOOOO");
+            }
         }
     }
 }
